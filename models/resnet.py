26b6094d582129a0c2705e17acf4adbabf514d7f
root <root@e3e3141007fb3011eb09e880eb00283505de-renxy738-0.e3e3141007fb3011eb09e880eb00283505de.0cd54cf9c48f4b0b3dfc4091082b1881.svc.cluster.local>
2021-03-08 10:33:37 +0800

    modified

--- models/resnet.py
+++ models/resnet.py
@@ -2,7 +2,7 @@
 
 import torch.nn as nn
 
-__all__ = ["resnet20", "resnet32", "resnet44", "resnet56", "resnet110", "resnet1202"]
+__all__ = ["resnet20", "resnet20_bnk", "resnet29_bnk", "resnet32", "resnet44", "resnet47_bnk", "resnet56", "resnet56_bnk", "resnet68", "resnet74_bnk", "resnet80", "resnet92", "resnet92_bnk", "resnet110", "resnet110_bnk", "resnet1202"]
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -85,7 +85,7 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, depth, num_classes, block_name="BasicBlock"):
+    def __init__(self, depth, layers, num_classes, block_name="BasicBlock"):
         super(ResNet, self).__init__()
         # Model type specifies number of layers for CIFAR-10 model
         if block_name == "BasicBlock":
@@ -107,9 +107,9 @@ class ResNet(nn.Module):
         self.conv_1 = nn.Conv2d(3, 16, kernel_size=3, padding=1, bias=False)
         self.bn_1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.stage_1 = self._make_layer(block, 16, n)
-        self.stage_2 = self._make_layer(block, 32, n, stride=2)
-        self.stage_3 = self._make_layer(block, 64, n, stride=2)
+        self.stage_1 = self._make_layer(block, 16, layers[0])
+        self.stage_2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.stage_3 = self._make_layer(block, 64, layers[2], stride=2)
         self.avgpool = nn.AvgPool2d(8)
         self.fc = nn.Linear(64 * block.expansion, num_classes)
 
@@ -160,24 +160,49 @@ class ResNet(nn.Module):
 
 
 def resnet20(num_classes):
-    return ResNet(depth=20, num_classes=num_classes)
+    return ResNet(depth=20, layers=[3,3,3], num_classes=num_classes)
 
+def resnet20_bnk(num_classes):
+        return ResNet(depth=20, layers=[2,2,2], num_classes=num_classes, block_name="Bottleneck")
+
+def resnet29_bnk(num_classes):
+    return ResNet(depth=29, layers=[3,3,3], num_classes=num_classes, block_name="Bottleneck")
 
 def resnet32(num_classes):
-    return ResNet(depth=32, num_classes=num_classes)
-
+    return ResNet(depth=32, layers=[5,5,5], num_classes=num_classes)
 
 def resnet44(num_classes):
-    return ResNet(depth=44, num_classes=num_classes)
+    return ResNet(depth=44, layers=[7,7,7], num_classes=num_classes)
 
+def resnet47_bnk(num_classes):
+    return ResNet(depth=47, layers=[5,5,5], num_classes=num_classes, block_name="Bottleneck")
 
 def resnet56(num_classes):
-    return ResNet(depth=56, num_classes=num_classes)
+    return ResNet(depth=56, layers=[9,9,9], num_classes=num_classes)
 
+def resnet56_bnk(num_classes):
+    return ResNet(depth=56, layers=[6,6,6], num_classes=num_classes, block_name="Bottleneck")
+
+def resnet68(num_classes):
+    return ResNet(depth=68, layers=[11,11,11], num_classes=num_classes)
+
+def resnet74_bnk(num_classes):
+    return ResNet(depth=74, layers=[8,8,8], num_classes=num_classes, block_name="Bottleneck")
+
+def resnet80(num_classes):
+    return ResNet(depth=80, layers=[13,13,13], num_classes=num_classes)
+
+def resnet92(num_classes):
+    return ResNet(depth=92, layers=[15,15,15], num_classes=num_classes)
+
+def resnet92_bnk(num_classes):
+    return ResNet(depth=110, layers=[10,10,10], num_classes=num_classes, block_name="Bottleneck")
 
 def resnet110(num_classes):
-    return ResNet(depth=110, num_classes=num_classes)
+    return ResNet(depth=110, layers=[18,18,18], num_classes=num_classes)
 
+def resnet110_bnk(num_classes):
+    return ResNet(depth=110, layers=[12,12,12], num_classes=num_classes, block_name="Bottleneck")
 
 def resnet1202(num_classes):
-    return ResNet(depth=1202, num_classes=num_classes)
+    return ResNet(depth=1202, layers=[200,200,200], num_classes=num_classes)
